e19709c37bcdcfa33224d7e458e3492382dd1478
viettungvuong <viettungvuong@gmail.com>
2022-04-04 17:02:47 +0700

    Chỉnh sửa code, tạo 2 function và cho chạy sau mỗi 0.5 phút (30 giây)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velaboost
+{
+    internal class Program
+    {
+        static int count = 0;
+        static void openVela()
+        {
+            var data = new WebClient().DownloadString("https://vela-academy.com/");
+            count++;
+            Console.WriteLine("vela-academy.com opened. Times: "+count.ToString());
+        }
+        static void Main()
+        {
+            openVela();
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(0.5);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                openVela();
+            }, null, startTimeSpan, periodTimeSpan);
+            Console.ReadKey();
+        }
+    }
+}
